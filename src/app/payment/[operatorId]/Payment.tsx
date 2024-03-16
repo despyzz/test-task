9e164686709cbc3ebf45b4be9e27e0cd4804f2df
yzz <yzzdespair@gmail.com>
2024-03-16 21:40:05 +0300

    add payment form skeleton

--- src/app/payment/[operatorId]/Payment.tsx
+++ src/app/payment/[operatorId]/Payment.tsx
@@ -9,7 +9,7 @@ import {Button} from "@/app/ui/common/Button";
 import {Container} from "@/app/ui/common/Container";
 import styled from "styled-components";
 import {device} from "@/app/lib/breakpoints";
-import {AmountInput, PhoneInput} from "@/app/ui/common/Input";
+import {AmountInput, InputSkeleton, PhoneInput} from "@/app/ui/common/Input";
 
 const COMPLETED_PHONE_NUMBER_LENGTH = 11;
 const MIN_AMOUNT = 1
@@ -33,6 +33,8 @@ const Form = styled.form`
 `
 
 const Title = styled.h1`
+    font-size: var(--font-m);
+    line-height: 1;
     color: var(--font-light)
 `
 
@@ -56,9 +58,56 @@ const Label = styled.label`
     gap: 5px;
 
     div {
+        line-height: 1;
         font-size: var(--font-m);
         color: var(--font-light);
     }
+`
+
+const FormSkeleton = styled.div`
+    margin-top: 30px;
+    background: radial-gradient(50% 50% at 50% 50%, #403A5F 0%, #211E2E 100%);
+    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+
+    padding: clamp(45px, 13vw, 90px) clamp(20px, 12.5vw, 100px);
+    border-radius: 40px;
+
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`
+
+const TitleSkeleton = styled.div`
+    width: clamp(100px, 30vw, 240px);
+    height: var(--font-m);
+    line-height: 1;
+    background: var(--font-light);
+    border-radius: 20px;
+`
+
+const InputTitleSkeleton = styled.div`
+    height: var(--font-m);
+    background: var(--font-light);
+    border-radius: 20px;
+    width: clamp(150px, 30vw, 240px);
+`
+
+const ButtonSkeleton = styled.div`
+    background: var(--font-light);
+    border-radius: 20px;
+    text-align: center;
+    color: transparent;
+
+    font-family: inherit;
+    transition: 0.3s;
+    background: var(--btn-light-bg);
+    font-weight: 600;
+    font-size: var(--font-s);
+    padding: 8px 12px;
+    width: fit-content;
+    outline: none;
+    border: none;
+    align-self: center;
 `
 
 export function Payment({id}: PaymentProps) {
@@ -66,6 +115,7 @@ export function Payment({id}: PaymentProps) {
 
   const [operator, setOperator] = useState<Operator | null>(null)
   const [fetchError, setFetchError] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const [phoneValue, setPhoneValue] = useState('');
   const [amountValue, setAmountValue] = useState('');
@@ -74,9 +124,11 @@ export function Payment({id}: PaymentProps) {
 
   const fetchData = (id: string) => {
     setFetchError(false);
+    setLoading(true);
 
     fetchOperator(id)
       .then(response => {
+        setLoading(false)
         if (!response.ok) throw new Error('Server error');
         return response.json();
       })
@@ -120,56 +172,77 @@ export function Payment({id}: PaymentProps) {
 
   return (
     <Container>
-      <Form>
-        <Title>Оператор: {operator?.name}</Title>
+      {
+        loading ?
+          <FormSkeleton>
+            <TitleSkeleton/>
+            <InputGroup>
+              <Label>
+                <InputTitleSkeleton/>
+                <InputSkeleton/>
+              </Label>
+              <Label>
+                <InputTitleSkeleton/>
+                <InputSkeleton/>
+              </Label>
+            </InputGroup>
 
-        <InputGroup>
-          <Label>
-            <div>Номер телефона:</div>
-            <PhoneInput
-              mask='+{7} (000) 000-00-00'
-              unmask={true}
-              lazy={false}
-              value={phoneValue}
-              onAccept={(value: string) => setPhoneValue(value)}
-              type="tel"
-            />
-          </Label>
+            <ButtonSkeleton>
+              Пополнить
+            </ButtonSkeleton>
+          </FormSkeleton>
+          :
+          <Form>
+            <Title>Оператор: {operator?.name}</Title>
 
-          <Label>
-            <div>Сумма:</div>
-            <AmountInput
-              value={amountValue}
-              onChange={({target: {value}}) => {
-                if (value === '') {
-                  setAmountValue('');
-                } else {
-                  const numValue = Number(value);
-                  if (numValue < MIN_AMOUNT) {
-                    setAmountValue(String(MIN_AMOUNT));
-                  } else if (numValue > MAX_AMOUNT) {
-                    setAmountValue(String(MAX_AMOUNT));
-                  } else {
-                    setAmountValue(value);
-                  }
-                }
+            <InputGroup>
+              <Label>
+                <div>Номер телефона:</div>
+                <PhoneInput
+                  mask='+{7} (000) 000-00-00'
+                  unmask={true}
+                  lazy={false}
+                  value={phoneValue}
+                  onAccept={(value: string) => setPhoneValue(value)}
+                  type="tel"
+                />
+              </Label>
+
+              <Label>
+                <div>Сумма:</div>
+                <AmountInput
+                  value={amountValue}
+                  onChange={({target: {value}}) => {
+                    if (value === '') {
+                      setAmountValue('');
+                    } else {
+                      const numValue = Number(value);
+                      if (numValue < MIN_AMOUNT) {
+                        setAmountValue(String(MIN_AMOUNT));
+                      } else if (numValue > MAX_AMOUNT) {
+                        setAmountValue(String(MAX_AMOUNT));
+                      } else {
+                        setAmountValue(value);
+                      }
+                    }
+                  }}
+                  type={"number"}
+                />
+              </Label>
+            </InputGroup>
+
+            <Button
+              align={'center'}
+              onClick={(event) => {
+                event.preventDefault();
+                onSendData();
               }}
-              type={"number"}
-            />
-          </Label>
-        </InputGroup>
-
-        <Button
-          align={'center'}
-          onClick={(event) => {
-            event.preventDefault();
-            onSendData();
-          }}
-          disabled={disabled}
-        >
-          Пополнить
-        </Button>
-      </Form>
+              disabled={disabled}
+            >
+              Пополнить
+            </Button>
+          </Form>
+      }
     </Container>
   );
 }
--- src/app/ui/common/Input.ts
+++ src/app/ui/common/Input.ts
@@ -9,7 +9,18 @@ const InputStyle = `
     background: transparent;
     color: var(--main-light);
     outline: none;
+    line-height: 1;
 `
+
+export const InputSkeleton = styled.div`
+    height: calc(46px + var(--font-m));
+    border-radius: 40px;
+    border: 2px solid var(--main-light);
+    padding: 20px 30px;
+    background: var(--main-light);
+    outline: none;
+`
+
 export const PhoneInput = styled(IMaskInput)`
     ${InputStyle}
 `
